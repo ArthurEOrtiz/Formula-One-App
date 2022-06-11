b0b75d6e045b9d1ec2a14e3c6a7864ecb2665c53
Sean Keane <seankeane93@gmail.com>
2022-06-10 20:02:55 -0700

    WIP added boxes to main html, trialed images for API calls, trialed creating/linking multiple html files.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Formula1Stats from './js/formula-one';
+// import Formula1Stats from './js/formula-one';
 
 $(document).ready(function(){
-  Formula1Stats.getStats()
-    .then(function(response){
-      console.log(response);
-      sessionStorage.setItem(response);
-    });
+  // Formula1Stats.getStats()
+  //   .then(function(response){
+  //     console.log(response);
+  //     sessionStorage.setItem(response);
+  //   });
 });
