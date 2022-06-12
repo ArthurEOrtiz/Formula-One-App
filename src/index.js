1000732145e045590f8f3634833287942161c72e
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-06-12 12:24:18 -0600

    Pulled driver stats
    testing session storage to pull driver stats

--- src/index.js
+++ src/index.js
@@ -7,11 +7,8 @@ import Formula1DriverStats from './js/formula-one';
 $(document).ready(function(){
   Formula1DriverStats.getDriverStats()
     .then(function(response){
-      const driverStatObject = response;
-      sessionStorage.setItem("driverStatObject",JSON.stringify(driverStatObject));
-      const driverS = sessionStorage.getItem("driverStatObject");
-      console.log(JSON.parse(driverS));
-  
-      //console.log(driverS.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+      const driverStatObject = response; // this response is already JSON
+      sessionStorage.setItem("driverStatObject",JSON.stringify(driverStatObject)); 
+      console.log(JSON.parse(sessionStorage.getItem('driverStatObject')).MRData.StandingsTable.StandingsLists[0].DriverStandings);
     });
 });
