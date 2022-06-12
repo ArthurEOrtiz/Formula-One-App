5ecd42bd0c338342d4f0d895ac544f9762915ace
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-06-12 12:08:29 -0600

    Stored API into Session Storage
    albe to store an object in sessionStorage with all the driver stats

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Formula1Stats from './js/formula-one';
+import Formula1DriverStats from './js/formula-one';
 
 $(document).ready(function(){
-// <<<<<<< CanadianRunner
-//   // Formula1Stats.getStats()
-//   //   .then(function(response){
-//   //     console.log(response);
-//   //     sessionStorage.setItem(response);
-//   //   });
-// =======
-//   Formula1Stats.getStats()
-//     .then(function(response){
-//       console.log(response);
-//     });
-// >>>>>>> main
+  Formula1DriverStats.getDriverStats()
+    .then(function(response){
+      const driverStatObject = response;
+      sessionStorage.setItem("driverStatObject",JSON.stringify(driverStatObject));
+      const driverS = sessionStorage.getItem("driverStatObject");
+      console.log(JSON.parse(driverS));
+  
+      //console.log(driverS.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+    });
 });
